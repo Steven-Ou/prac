173dc76f301ec31a481f77723f921bfec831d8f0
Steven-Ou <osteve425@gmail.com>
2024-09-04 13:50:08 -0400

    problem 4 while and forloop

--- prac.cpp
+++ prac.cpp
@@ -13,3 +13,22 @@ int main(){
     }
     return 0;
 }*/
+
+int main(){
+    int n;
+    cout<<"Enter a positive integer: ";
+    cin>>n;
+
+    int whilesum =0, whileCount =1;
+    while(whileCount<= n){
+        whileSum+= whileCount;
+        whileCount++;
+    }
+    cout<<"Sum using while loop: " <<whileSum <<endl;
+    int forSum =0;
+    for(int i =1; i<=n; i++){
+        forSum +=i;
+    }
+    cout<<"Sum using for loop: "<< forSum<< endl;
+    return 0;
+}
