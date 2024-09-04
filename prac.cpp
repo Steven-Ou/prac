d55b3cca7655a9d08e6ad3a5e51342c1d7ed5696
Steven-Ou <osteve425@gmail.com>
2024-09-04 13:46:16 -0400

    even and odd test

--- prac.cpp
+++ prac.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main(){
     int num1, num2;
-    cout<<
+    cout<< "Enter a number: ";
+    cin>> num; 
+    if(num%2 ==0 ){
+        cout<<"even"<<endl;
+    }else{
+        cout<<"odd"<<endl;
+    }
+    return 0;
 }
