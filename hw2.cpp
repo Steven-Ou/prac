58824d4e4dd63a61469c08d6c9a645e84e64783f
Steven-Ou <osteve425@gmail.com>
2024-09-12 00:51:13 -0400

    fixing the structures of the loops, to avoid confusion

--- hw2.cpp
+++ hw2.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main(){
     int win[6];
-    for(int i0=0;i0<10;i0++){
+    for(int i0=0; i0<10; i0++){
         win[0]=i0;
-        for(int i1=0;i1<10;i1++){
+        for(int i1=0; i1<10; i1++){
             win[1]=i1;
-            for(int i2=0;i2<10;i2++){
+            for(int i2=0; i2<10; i2++){
                 win[2]=i2;
-                for(int i3=0;i3<10;i3++){
+                for(int i3=0; i3<10; i3++){
                     win[3]=i3;
-                    for(int i4=0;i4<10;i4++){
+                    for(int i4=0; i4<10; i4++){
                         win[4]=i4;
-                        for(int i=0;i<5;i++){
+                        for(int i=0; i<5; i++){
                             cout<<win[i];
                         }
                         cout<<endl;
