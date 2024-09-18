8de8de2e43a3001420c4b3b5e78cd7793d7e4f8e
Steven-Ou <osteve425@gmail.com>
2024-09-18 14:01:36 -0400

    for loop plus formula

--- hw3.cpp
+++ hw3.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main(){
-    string outcome="";
-    while(outcome != "done"){
+    string result="";
+    while(result != "done"){
         int team[26]={0};
         double score[26]={0};
+        for(int i=0;i<result.length();i++){
+            team[result[i]-'A']++;
+        }
     }
 }
