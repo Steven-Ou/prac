216eacb8b7d710435439440585dedf9e5d970cea
Steven-Ou <osteve425@gmail.com>
2024-09-20 23:53:29 -0400

    changing the condition for the while loop

--- hw3.cpp
+++ hw3.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(){
     string result;
-    while(result != "done"){
+    while(true){
+
         int team[26]={0};
         double score[26]={0};
         for(int i=0;i<result.length();i++){
