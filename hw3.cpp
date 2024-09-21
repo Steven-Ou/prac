124a9537f68611aa1239251d636ec315a01789dd
Steven-Ou <osteve425@gmail.com>
2024-09-20 23:52:57 -0400

    fixing

--- hw3.cpp
+++ hw3.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main(){
-    string result="";
-    cout<<"Enter a word";
-    cin>>result;
+    string result;
     while(result != "done"){
         int team[26]={0};
         double score[26]={0};
