f3b7ab2ffaadac4f591739b1ea780ae76752c300
Steven-Ou <osteve425@gmail.com>
2024-09-30 14:33:30 -0400

    conflicts

--- project4.cpp
+++ project4.cpp
@@ -1,7 +1,17 @@
 //Eight number in a cross
 #include <iostream>
 using namespace std;
+int adj[8][5]={
+    {-1},
+    {0,-1},
+    {0,-1},
+    {0,1,2,-1},
+    {0,1,3,-1},
+    {1,4,-1},
+    {2,3,4,-1},
+    {3,4,5,6,-1}
+};
 int main(){
-    
+
    return 0; 
 }
