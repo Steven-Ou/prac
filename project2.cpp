207d2f76233f01683d346a7af529b33f534592dd
Steven-Ou <osteve425@gmail.com>
2024-10-02 01:24:01 -0400

    fixed

--- project2.cpp
+++ project2.cpp
@@ -1,48 +1,48 @@
-//1d array 
 #include <iostream>
 using namespace std;
-int main(){
-   int q[8]={0};
-   int shu =0;//count
-   int daan=0;//solution
 
-   nextCo:
-   shu++;
-   if(shu==8){
-      goto print;
-   }
-   q[shu] = -1;
+int main() {
+    int q[8]={0};
+    int shu=0; // count
+    int daan=0; // solution
 
-   nextR:
-   q[shu]++;
-   if(q[shu]==8){
-      goto backtrack;
-   }
-   for(int i=0;i<shu;i++){
-      if(q[shu]==q[i] || shu-i==q[shu]|| shu-i== abs(q[shu]-q[i]) ){
-         goto nextR;
-      }
-   }
-   goto nextCo;
+    nextCo:
+    shu++;
+    if (shu==8) {
+        goto print;
+    }
+    q[shu] = -1;
 
-   backtrack:
-   shu--;
-   if(shu==-1){
-      return 0;
-   }
-   goto nextR;
+    nextR:
+    q[shu]++;
+    if (q[shu] == 8) {
+        goto backtrack;
+    }
+    for (int i = 0; i < shu; i++) {
+        if (q[shu] == q[i] || shu - i == abs(q[shu] - q[i])) {
+            goto nextR;
+        }
+    }
+    goto nextCo;
 
-   print:
-   cout<<"Solution"<< ++daan<<": \n ";
-   for(int a=0;a<8;a++){
-      for(int b=0;b<8;b++){
-         if(q[a]==b){
-            cout<<"1";
-         }else{
-            cout<<"0";
-         }
-      }
-      cout<<endl;
-   }
-   goto backtrack;
+    backtrack:
+    shu--;
+    if (shu == -1) {
+        return 0;
+    }
+    goto nextR;
+
+    print:
+    cout << "Solution " << ++daan << ": \n";
+    for (int a = 0; a < 8; a++) {
+        for (int b = 0; b < 8; b++) {
+            if (q[a] == b) {
+                cout << "1 ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+    goto backtrack;
 }
