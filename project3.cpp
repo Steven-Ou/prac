811161408179fc444ee184fa552e01273f9c6d8d
Steven-Ou <osteve425@gmail.com>
2024-10-02 23:29:37 -0400

    The final of the codes

--- project3.cpp
+++ project3.cpp
@@ -37,7 +37,33 @@ int main(){
 
             q[2]=a2;
 
-            
+            for(int a3=0; a3<8;a3++){
+
+               q[3]=a3;
+
+               for(int a4=0; a4<8; a4++){
+
+                  q[4]=a4;
+
+                  for(int a5=0; a5<8; a5++){
+
+                     q[5]=a5;
+
+                     for(int a6=0; a6<8; a6++){
+
+                        q[6]=a6;
+
+                        for( int a7=0; a7<8; a7++){
+                           
+                           q[7]=a7;
+                           if(ok(q)){
+                              printB(q); 
+                           }
+                        }
+                     }
+                  }
+               }
+            }
          }
       }
    }
