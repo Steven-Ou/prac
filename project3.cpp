3bf500cfd0088e9c50bd5078591686f280da8dd5
Steven-Ou <osteve425@gmail.com>
2024-10-02 22:48:38 -0400

    creating the printing function

--- project3.cpp
+++ project3.cpp
@@ -7,14 +7,17 @@ int ans = 0;
 bool ok(int b[]){
    for(int a=0; a<7; a++){
       for(int c=a+1;c<8;c++){
-         if(b[a]==b[c]||c-a == abs(b[c]-b[a])){
-            return false; 
+         if(b[a]==b[c]||c-a == abs(b[c]-b[a])){ // checks if the queens are in the same diagonal
+            return false;  // checks if the queens are in the same row
          }
       }
    }
    return true;  // when everything is correct returns true
 }
 
+void print(int c[]){
+      
+}
 int main(){
    return 0; 
 }
