e4fc0fa97cf955b52b948142bba53c1601f6e406
Steven-Ou <osteve425@gmail.com>
2024-10-02 22:42:04 -0400

    created the okay function

--- project3.cpp
+++ project3.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 int ans = 0; 
 
+bool ok(int b[]){
+   for(int a=0; a<7; a++){
+      for(int b=a+1;b<8;b++){
+         if(b[a]==b[b]||b-i == abs(b[b]-b[i])){
+            return false; 
+         }
+      }
+   }
+}
 int main(){
    return 0; 
 }
