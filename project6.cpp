81ee48f91ef60180a7e24f789c84f24ad1f7eeab
Steven-Ou <osteve425@gmail.com>
2024-10-15 14:00:23 -0400

    blocks

--- project6.cpp
+++ project6.cpp
@@ -1,1 +1,23 @@
 //Stable Marriage
+#include <iostream>
+using namespace std;
+int mp[3][3] = {
+    {0,2,1},
+    {0,2,1},
+    {1,2,0}
+};
+int wp[3][3] = {
+    {2,1,0},
+    {0,1,2},
+    {2,0,1}
+};
+bool ok(int b[], int c){
+    return true;
+}
+
+void print(int b[]){
+
+}
+int main(){ 
+    return 0; 
+}   
