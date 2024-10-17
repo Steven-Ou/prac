b88fc0529495bae60e6f64f1a55fae101c518a3b
Steven-Ou <osteve425@gmail.com>
2024-10-16 21:28:31 -0400

    new

--- project4.cpp
+++ project4.cpp
@@ -1,6 +1,7 @@
 //Eight number in a cross
 #include <iostream>
 using namespace std;
+//adjacents
 int adj[8][5]={
     {-1},
     {0,-1},
@@ -11,6 +12,7 @@ int adj[8][5]={
     {2,3,4,-1},
     {3,4,5,6,-1}
 };
+int count =0; //count variable. 
 int main(){
 
    return 0; 
