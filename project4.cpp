7ab75c7ef0abb5ab5d2e117f3115ba8ef727de34
Steven-Ou <osteve425@gmail.com>
2024-10-17 20:52:21 -0400

    comments

--- project4.cpp
+++ project4.cpp
@@ -12,7 +12,7 @@ int adj[8][5]={
     {2,3,4,-1},
     {3,4,5,6,-1}
 };
-int count =0; //count variable. 
+int count =0; //used for counting the number of solution
 
 bool ok(int cross[], int cap){
     for(int i=0;i<cap;i++){
