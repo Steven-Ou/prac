823959bf023be63d74ef77c7865e5cfa3f16bd7f
Steven-Ou <osteve425@gmail.com>
2024-10-17 20:57:00 -0400

    comments

--- project4.cpp
+++ project4.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 //helper array
-int adj[8][5]={
-    {-1},
+int adj[8][5]={//This is used to represent a position of each index. 
+    {-1},//end of each row.
     {0,-1},
     {0,-1},
     {0,1,2,-1},
@@ -16,12 +16,12 @@ int count =0; //used for counting the number of solution
 
 bool ok(int cross[], int cap){
     for(int i=0;i<cap;i++){
-        if( cross[i] == cross[cap]){//checking if the number
-            return false;           //is on the same line
+        if( cross[i] == cross[cap]){//checking if the number repeats
+            return false;           
         }
     }
-    for(int j=0; adj[cap][j] !=-1; j++){    //As long as the 
-        if(abs(cross[cap]-cross[adj[cap][j]])==1){
+    for(int j=0; adj[cap][j] !=-1; j++){    //Looks through all adjacent positions of 'cap'
+        if(abs(cross[cap]-cross[adj[cap][j]])==1){// if adjacent numbers differ by 1, its false
             return false; 
         }
     }
