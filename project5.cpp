be05a57a1554d33733d783a845e627fe8c134081
Steven-Ou <osteve425@gmail.com>
2024-10-19 00:57:45 -0400

    comments

--- project5.cpp
+++ project5.cpp
@@ -6,6 +6,10 @@ int count =0; //counting variable in the universe.
 //Ok function is used to check if placing a queen in column is valid.
 bool ok(int b[], int c){
     for(int i=0;i<c;i++){//Loop through to check if the queens have conflicts.
-        
+       //Check if the queens are in column and is in the same row
+       if(b[c]==b[i]||c-i==abs(b[c]-b[i])){//Also checks if they are on the same diagonal.
+            return false;
+       } 
     }
+   
 }
