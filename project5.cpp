9bfcd9862e249ff4b4038a6c6a3ddac9afacf9a4
Steven-Ou <osteve425@gmail.com>
2024-10-19 00:43:58 -0400

    creating for loop

--- project5.cpp
+++ project5.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int count =0; //counting variable in the universe.
 //Ok function is used to check if placing a queen in column is valid.
 bool ok(int b[], int c){
-
+    for(int i)
 }
