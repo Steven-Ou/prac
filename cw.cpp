da46c5249261ae0f97d40a35d796c23d9e55e234
Steven-Ou <osteve425@gmail.com>
2024-10-21 14:10:09 -0400

    Without pointer method

--- cw.cpp
+++ cw.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std; 
 
+/* 
+//Without pointer 
 void swap(int a[],int b[], int size){
-    
+    for(int i =0; i< size; i++){
+        int temp;
+        temp = a[i]; 
+        a[i] = b[i];
+        b[i]= temp; 
+    }
+    return; 
+       
 }
+void print(int a[], int size){
+    for( int i =0; i<size; i++){
+        cout<<a[i]<< " ";
+    }
+    cout<<endl;
+}
+*/
