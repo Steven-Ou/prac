9bf3129163e99332ae33c7a42407dd8c4368b24e
Steven-Ou <osteve425@gmail.com>
2024-10-22 00:01:40 -0400

    spacing

--- project6.cpp
+++ project6.cpp
@@ -20,6 +20,7 @@ bool ok(int b[], int c){
         if(b[i]==b[c]){
             return false;
         }
+    
         if((mp[i][b[c]]<mp[i][b[i]]) && (wp[b[c]][i]<wp[b[c]][c])){
             return false; 
         }
