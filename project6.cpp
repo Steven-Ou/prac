69741591e0a17c0f43f7a11e9fcbd7c4a2669d12
Steven-Ou <osteve425@gmail.com>
2024-10-22 00:01:01 -0400

    fixing

--- project6.cpp
+++ project6.cpp
@@ -16,7 +16,7 @@ int wp[3][3] = {//Women's preference list
 bool ok(int b[], int c){
     //loop through all men before men c to check if the paring is valid
     for(int i =0;i<c;i++){ 
-        if(b[i]==b[c]){//check if any two men are paired with the same woman
+        if(b[i]==b[c]){
             return false;
         }
         if((mp[i][b[c]]<mp[i][b[i]]) && (wp[b[c]][i]<wp[b[c]][c])){
