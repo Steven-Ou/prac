4a091c5151e96b41524ce954952b645c83345c03
Steven-Ou <osteve425@gmail.com>
2024-10-21 23:57:55 -0400

    structure

--- project6.cpp
+++ project6.cpp
@@ -12,6 +12,7 @@ int wp[3][3] = {//Women's preference list
     {0,1,2},// the pattern should be understandable!
     {2,0,1}// it's similar to the man ways too!
 };
+
 bool ok(int b[], int c){
     for(int i =0;i<c;i++){
         if(b[i]==b[c]){
