e314056bd26ee59787f4bb027d2aefaeade0f8e4
Steven-Ou <osteve425@gmail.com>
2024-10-22 00:04:40 -0400

    comments

--- project6.cpp
+++ project6.cpp
@@ -25,7 +25,7 @@ bool ok(int b[], int c){
         if((mp[i][b[c]]<mp[i][b[i]]) && (wp[b[c]][i]<wp[b[c]][c])){
             return false; 
         }
-    //
+    //Check if man c prefers woman (b[i]) over his current match
         if((mp[c][b[i]]<mp[c][b[c]])&&(wp[b[i]][c]<wp[b[i]][i])){
             return false;
         }
