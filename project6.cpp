c8457eadf41b794e254a4a53864c7decc349250d
Steven-Ou <osteve425@gmail.com>
2024-10-22 00:11:28 -0400

    commenting

--- project6.cpp
+++ project6.cpp
@@ -50,7 +50,7 @@ void print(int b[]){
 int main(){ 
     int q[3]={};//Stores the current matchings (q[i] is the woman paried with man i)
     int c=0;// c is the index of the current man being paired
-    q[c]=0;
+    q[c]=0;//Initialize the first pairing with woman 0. 
     while(c>=0){
         c++;
         if(c==3){
