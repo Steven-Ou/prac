3e55bab2f2dd5aafab6915e47593f885a26213d2
Steven-Ou <osteve425@gmail.com>
2024-10-30 14:18:48 -0400

    count var

--- project7.cpp
+++ project7.cpp
@@ -1,7 +1,7 @@
 //Fancy 8 queen
 #include <iostream>
 using namespace std;
-int shu; //Global count variable
+int shu=0; //Global count variable
 void print(){
    cout<< "Solution #: "<< ++shu<<endl; 
 }
