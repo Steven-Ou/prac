622380e166a9aceb62e9be027afdb77bdb4b306e
Steven-Ou <osteve425@gmail.com>
2024-10-30 14:20:18 -0400

    Global var

--- project7.cpp
+++ project7.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 int shu=0; //Global count variable
+typedef char box[5][7];
 void print(){
    cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
-   //Printing the board 
+   //Printing the board
+   for() 
 
 }
 int main(){
