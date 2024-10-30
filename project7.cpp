f0b53b318a7ac39b92a89631be7f2bc13e3be6a1
Steven-Ou <osteve425@gmail.com>
2024-10-30 14:25:46 -0400

    comments

--- project7.cpp
+++ project7.cpp
@@ -8,7 +8,7 @@ void print(){
     cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
     //Printing the board
     for(int i=0;i<8;i++){ //for each board row
-        for(int k=0; k<5;k++){//
+        for(int k=0; k<5;k++){//for each box row 
             cout<<char(179);
         }    
 
