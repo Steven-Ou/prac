be70ac0451a8c7bb3f37b00e23032ff143b82dff
Steven-Ou <osteve425@gmail.com>
2024-10-30 14:19:32 -0400

    comment

--- project7.cpp
+++ project7.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int shu=0; //Global count variable
 void print(){
    cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
+   //Printing the board 
 
 }
 int main(){
