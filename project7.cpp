f598d834cd0c1d51ead9ffe1c9f4b06044e62f33
Steven-Ou <osteve425@gmail.com>
2024-10-30 14:24:20 -0400

    fixing

--- project7.cpp
+++ project7.cpp
@@ -5,13 +5,10 @@ int shu=0; //Global count variable
 typedef char box[5][7];
 box bb, wb, *board[8][8];
 void print(){
-<<<<<<< HEAD
     cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
     //Printing the board
     for(int i=0;i<8;i++){
-=======
-
->>>>>>> 8f25913fe907b20325d324987420d1781b51124a
+        
 
     }
 }
