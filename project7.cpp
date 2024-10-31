04a668a33d9ddd7cadff11febb663395058671d3
Steven-Ou <osteve425@gmail.com>
2024-10-30 23:41:08 -0400

    p[r][c]

--- project7.cpp
+++ project7.cpp
@@ -33,6 +33,8 @@ int main(){
     int p[8][8]={0};
     //Start in the first row, first column, and place a queen there 
     int r= 0,c=0; 
+    p[r][c] = 1; 
+    
 print:
     for(int i=0;i<8;i++){
         for(int j=0; j<8;j++){
