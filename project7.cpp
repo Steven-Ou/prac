84d28c3d0aec274d6001d8cc96dc4331ba6b1d8e
Steven-Ou <osteve425@gmail.com>
2024-10-30 23:36:23 -0400

    bq

--- project7.cpp
+++ project7.cpp
@@ -17,10 +17,11 @@ int main(){
                 wq[i][j]= char(219);
             }else if(i==1){
                 if(j%2==0){
-                   bq[i][j]= ' ';
-                   wq[i][j]= char(219);  
+                    bq[i][j]= ' ';
+                    wq[i][j]= char(219);  
                 }else{
-                    
+                    bq[i][j]= char(219);
+                    wq[i][j]= ' ';
                 }
             }
         }
