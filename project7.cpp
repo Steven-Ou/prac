089880c676b6092b36560d4f5876bfe10d8a9646
Steven-Ou <osteve425@gmail.com>
2024-10-30 20:45:24 -0400

    codes for printing the board with a pointer

--- project7.cpp
+++ project7.cpp
@@ -12,9 +12,10 @@ void print(){
             cout<<char(179);
             for(int j=0; j<8;j++){//each board column
                 for(int l=0;l<7;l++){//each box column
-                    
+                    cout<<(*board[i][k])[j][l];
                 }
             }
+            cout<<char
         }    
 
     }
