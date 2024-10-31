8a230cb2e2b916dbcb7ea133e3500edd6c480527
Steven-Ou <osteve425@gmail.com>
2024-10-30 21:25:09 -0400

    created if statements with conditions and a forloop

--- project7.cpp
+++ project7.cpp
@@ -6,7 +6,11 @@ typedef char box[5][7];
 box bb, wb, *board[8][8];
 print:
     for(int i=0;i<8;i++){
-        for
+        for(int j=0; j<8;j++){
+            if((i+j)%2==0){
+                
+            }
+        }
     }
     cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
     //Printing the board
