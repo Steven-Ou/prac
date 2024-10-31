cec693c48dc94442ee6a6a53bda1d36372d43b72
Steven-Ou <osteve425@gmail.com>
2024-10-30 21:23:53 -0400

    fixing it to

--- project7.cpp
+++ project7.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int shu=0; //Global count variable
 typedef char box[5][7];
 box bb, wb, *board[8][8];
-void print(){
+print:
+
     cout<< "Solution #: "<< ++shu<<endl; //printing out the number of solution
     //Printing the board
     for(int i=0;i<8;i++){ //for each board row
