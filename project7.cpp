30d12806e4c1cc3e5bd4a0a7089e0e5d72643ab8
Steven-Ou <osteve425@gmail.com>
2024-11-02 01:18:29 -0400

    fixing

--- project7.cpp
+++ project7.cpp
@@ -63,11 +63,11 @@ nextRow:
             }
         }
     //If there is a queen in the lower left diagonal, goto NextRow
-    for(int i=1;r+i<8 && c-i>-1;i++){
-        if(p[r+i][c-i]==1){
-            goto nextRow;
+        for(int i=1;r+i<8 && c-i>-1;i++){
+            if(p[r+i][c-i]==1){
+                goto nextRow;
+            }
         }
-    }
     //Place a queen in the current square
     p[r][c]=1;
     //Goto nextCol
