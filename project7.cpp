552c6a1bc8ee5d0a8af3a33fff9c0a01ffe99676
Steven-Ou <osteve425@gmail.com>
2024-11-02 01:00:55 -0400

    p array inside the if statement

--- project7.cpp
+++ project7.cpp
@@ -51,8 +51,8 @@ nextRow:
     }
     //If there is a queen in the same row(to the left), goto nextRow
     for(int i =0; i<c;i++){
-       if(){
-        
+       if(p[][]){
+
        } 
     }
 backtrack:
