730bf56a3bdd38ee31557fed4838b62f9a27cbda
Steven-Ou <osteve425@gmail.com>
2024-11-02 01:35:10 -0400

    comments

--- project7.cpp
+++ project7.cpp
@@ -84,7 +84,7 @@ backtrack:
         while(p[r][c]!=1){
             r++;
         }
-    
+    //Remove the queen from the current square
 print:
     for(int i=0;i<8;i++){
         for(int j=0; j<8;j++){
