72028d323155c4b7274faa7624ff6fa323241cf1
Steven-Ou <osteve425@gmail.com>
2024-11-03 00:26:55 -0400

    comments

--- project7.cpp
+++ project7.cpp
@@ -119,7 +119,7 @@ print:
         }
     }
     cout<<"┐\n";
-    
+    //Print ech row with side borders
     for(int i=0;i<8;i++){ //for each board row
         for(int k=0; k<5;k++){//for each box row 
             for(int j=0; j<8;j++){//each board column
