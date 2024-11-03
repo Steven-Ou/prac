0965d94f28f0340c39bd4b6742e5b7b3e6ca4d51
Steven-Ou <osteve425@gmail.com>
2024-11-03 00:23:48 -0400

    ┐\n

--- project7.cpp
+++ project7.cpp
@@ -118,7 +118,7 @@ print:
             cout<<"┬";//Column separator for top border
         }
     }
-    cout<<"";
+    cout<<"┐\n";
     for(int i=0;i<8;i++){ //for each board row
         for(int k=0; k<5;k++){//for each box row 
             for(int j=0; j<8;j++){//each board column
