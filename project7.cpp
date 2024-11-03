685b6e8557cdc9e5890d7760a6a297b3d7228665
Steven-Ou <osteve425@gmail.com>
2024-11-03 00:21:22 -0400

    cout

--- project7.cpp
+++ project7.cpp
@@ -115,7 +115,7 @@ print:
     for(int j=0;j<8;j++){
         cout<<"───────";//Top of each square
         if(j<7){
-            
+            cout<<"";
         }
     }
     for(int i=0;i<8;i++){ //for each board row
