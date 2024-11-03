78b08f5e0f4b2677220f049c05f5d6855bc586eb
Steven-Ou <osteve425@gmail.com>
2024-11-03 00:20:25 -0400

    7 - lines

--- project7.cpp
+++ project7.cpp
@@ -113,7 +113,7 @@ print:
     //Top border of the board
     cout<<"┌";
     for(int j=0;j<8;j++){
-        cout<<"";
+        cout<<"───────";
     }
     for(int i=0;i<8;i++){ //for each board row
         for(int k=0; k<5;k++){//for each box row 
