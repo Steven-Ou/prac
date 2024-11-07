625af004234e6bdb8a04eb31ab7755e60d669fb8
Steven-Ou <osteve425@gmail.com>
2024-11-07 00:39:38 -0500

    fixed

--- vector_exercise.cpp
+++ vector_exercise.cpp
@@ -7,7 +7,7 @@ int main(){
     double sum =0, avg = 0; 
     cout<<"Enter the number of classes their are : \n";
     cin>>c;
-    vector<vector<int>> v(c); 
+    vector<vector<int>>v(c); 
     for(int i =0;i<c;i++){
         cout<<"Enter the score of the student in class "<<i+1<<": ";
         cin>>cS; 
