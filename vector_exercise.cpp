51561ba5b34931bc2f57889ee1670ba311f2e208
Steven-Ou <osteve425@gmail.com>
2024-11-07 01:20:30 -0500

    can round

--- vector_exercise.cpp
+++ vector_exercise.cpp
@@ -29,7 +29,7 @@ int main(){
                 avg++;
             }
         }
-        cout<< setprecision(3)<<(avg/(v[i].size()-1))*100.000<<" % " <<endl;
+        cout<< setprecision(5)<<(avg/(v[i].size()-1))*100.000<<" % " <<endl;
         sum=0;
         avg=0;
     }
