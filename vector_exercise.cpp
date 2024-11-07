f1c8e33dbff6dae50bf5dfbe4acf82580dcd7e42
Steven-Ou <osteve425@gmail.com>
2024-11-06 23:36:28 -0500

    i+1

--- vector_exercise.cpp
+++ vector_exercise.cpp
@@ -9,7 +9,7 @@ int main(){
     cin>>c;
     vector<vector<int>> v(c); 
     for(int i =0;i<c;i++){
-        cout<<"Enter the score of the student in class "<<i<<": ";
+        cout<<"Enter the score of the student in class "<<i+1<<": ";
         cin>>cS; 
         v[i].push_back(cS);
         for(int j = 0; j<cS;j++){
