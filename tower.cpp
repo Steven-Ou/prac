2637be2650855ab04c1b3f66e2e7aebfa17c07d5
Steven-Ou <osteve425@gmail.com>
2024-11-11 14:24:28 -0500

    comments

--- tower.cpp
+++ tower.cpp
@@ -3,5 +3,6 @@
 using namespace std; 
 
 int main(){
-    vector<int> t[3];//3 towers a.b.c represented as an array of 3
+    vector<int> t[3];//3 towers a.b.c represented as an array of 3 vectors
+    
 }
