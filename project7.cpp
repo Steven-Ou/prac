0281812226666b4dfeab206c0455d0c6fb7f7f25
Steven-Ou <osteve425@gmail.com>
2024-11-18 14:00:09 -0500

    new block new version

--- project7.cpp
+++ project7.cpp
@@ -103,7 +103,7 @@ print: // Label for printing the board
     }
 
     cout << "Solution # " << ++shu << ":\n"; // Print the solution number
-    cout <<"+"; // Print the top border
+    cout <<"\u2594"; // Print the top border
     for(int col=0; col<8; col++){ // Loop through each column
         cout << "-------"; // Print dashes for each column width
         if(col<8){ 
