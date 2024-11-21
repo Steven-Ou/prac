dfed28a6659e5d7bd127ec1862d969032bd9a200
Steven-Ou <osteve425@gmail.com>
2024-11-20 23:28:28 -0500

    statement

--- Integration.cpp
+++ Integration.cpp
@@ -20,4 +20,5 @@ double cube(double x){
 
 int main(){
     cout<<"The integral of f(x)=x between 1 and 5 is: "<< integrate(line,1,5)<<endl;
+    cout<<"The integral of f(x)=x^2 between 1 and 5 is: "
 }
