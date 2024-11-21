c790501ef7dd81cf3d9b8d45802beab3411b831e
Steven-Ou <osteve425@gmail.com>
2024-11-21 00:12:12 -0500

    comments

--- Integration.cpp
+++ Integration.cpp
@@ -32,7 +32,7 @@ double cube(double x){
 }
 
 int main(){
-    //Set precision for floating-point output to 6 decimal places.
+    //Set precision to 1 so it can round to it's closes answer!
     cout<<fixed<<setprecision(1);
     //displaying and computing the integral of f(x)=x from 1 to 5.
     cout<<"The integral of f(x)=x between 1 and 5 is: "<< integrate(line,1,5)<<endl;
