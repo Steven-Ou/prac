9338f76a449a40ff64a1d691f49ae4bdc5f7c12b
Steven-Ou <osteve425@gmail.com>
2024-11-20 21:55:40 -0500

    comments

--- project8.cpp
+++ project8.cpp
@@ -7,7 +7,7 @@ int main(){
     //Declaring an array of 3 vectors to represents each tower
     vector <int> tower[3];
     int from =0;// set to 0 to start at tower A
-    int to =1; 
+    int to =1; //set to 1 so it can move to the next destination tower B.
     int candit =1;
     int move =0;
     int n; //Ring
