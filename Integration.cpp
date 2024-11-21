19cb54a7b1f44dbdd3c35c36d15de1b57ebf0fc4
Steven-Ou <osteve425@gmail.com>
2024-11-20 23:52:41 -0500

    comments

--- Integration.cpp
+++ Integration.cpp
@@ -37,6 +37,6 @@ int main(){
     cout<<"The integral of f(x)=x^2 between 1 and 5 is: "<< integrate(square,1,5)<<endl;
     //displaying and computing the integral of f(x)=x^3 from 1 to 5.
     cout<<"The integral of f(x)=x^3 between 1 and 5 is: "<<integrate(cube,1,5)<<endl;
-    //
+    //Exiting the program
     return 0;
 }
