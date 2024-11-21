bed70f61b32fafa693f6536d0feb795a072e22dc
Steven-Ou <osteve425@gmail.com>
2024-11-20 23:28:45 -0500

    recursive call

--- Integration.cpp
+++ Integration.cpp
@@ -20,5 +20,5 @@ double cube(double x){
 
 int main(){
     cout<<"The integral of f(x)=x between 1 and 5 is: "<< integrate(line,1,5)<<endl;
-    cout<<"The integral of f(x)=x^2 between 1 and 5 is: "
+    cout<<"The integral of f(x)=x^2 between 1 and 5 is: "<< integrate(square,1,5)
 }
