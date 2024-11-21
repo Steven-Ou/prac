ce0a72954a0bfe1bb1c6ed14a6f20da8290765fd
Steven-Ou <osteve425@gmail.com>
2024-11-20 23:51:20 -0500

    comments

--- Integration.cpp
+++ Integration.cpp
@@ -31,7 +31,7 @@ double cube(double x){
 }
 
 int main(){
-    //displaying and computing the integral of f(x)=x 
+    //displaying and computing the integral of f(x)=x from 1 to 5.
     cout<<"The integral of f(x)=x between 1 and 5 is: "<< integrate(line,1,5)<<endl;
     cout<<"The integral of f(x)=x^2 between 1 and 5 is: "<< integrate(square,1,5)<<endl;
     cout<<"The integral of f(x)=x^3 between 1 and 5 is: "<<integrate(cube,1,5)<<endl;
