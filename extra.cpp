c62bded598e9b47d5b4e787eb8850e4416455128
Steven-Ou <osteve425@gmail.com>
2024-11-25 13:58:13 -0500

    condition

--- extra.cpp
+++ extra.cpp
@@ -1,3 +1,12 @@
 //Extra Credit Assignment
 #include <iostream>
 using namespace std; 
+int main(){
+    int n; 
+    cout<<"Enter a number: ";
+    cin>>n;
+    while(n<1){
+        cout<<"Number can't be less than 1\n";
+        cin>>n;
+    }
+}
