479cd051ca91d4b64513c641ec63214e63032f17
Steven-Ou <osteve425@gmail.com>
2024-11-25 14:24:13 -0500

    commenting

--- extra.cpp
+++ extra.cpp
@@ -11,6 +11,7 @@ int main(){
         //Printing a warning also asking the user to enter a number again
         cin>>n;
     }
+    //
     int** arr = new int *[n];
     for(int i=0; i<n;i++){
         arr[i]= new int[n]; 
