bac18dae7957bb86015466657f54cde81a9bc032
Steven-Ou <osteve425@gmail.com>
2024-11-25 14:22:39 -0500

    commenting

--- extra.cpp
+++ extra.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Enter a number: ";
     //Asking the user with a speech
     cin>>n;
-    while(n<1){//
+    while(n<1){//Condition checking while loop
         cout<<"Number can't be less than 1\n";
         cin>>n;
     }
