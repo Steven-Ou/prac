95655b1abd603a2683d59bf284f382b3015aafec
Steven-Ou <osteve425@gmail.com>
2024-12-04 21:38:19 -0500

    QUiz

--- quiz.cpp
+++ quiz.cpp
@@ -46,3 +46,58 @@ int main(int argc, const char * argv[]) {
 
 }
 */
+int main()
+{
+    int classCount, classSize, maxI = 0;
+    cout << "Enter the number of classes: ";
+    cin >> classCount;
+    cout << "Enter the number of students in each class: ";
+    cin >> classSize;
+    int** scores = new int*[classCount];
+    for (int i = 0; i < classCount; i++)
+    {
+        scores[i] = new int[classSize];
+        cout << "Enter the scores of class " << i+1 << ": ";
+        for (int j = 0; j < classSize; j++)
+        {
+            cin >> scores[i][j];
+        }
+    }
+    for (int i = 0; i < classCount; i++)
+    {
+        cout << "Scores of students in class " << i+1 << ": ";
+        for (int j = 0; j < classSize; j++)
+        {
+            cout << scores[i][j] << " ";
+        }
+        cout << endl;
+    }
+    double* averages = new double[classCount];
+    for (int i = 0; i < classCount; i++)
+    {
+        cout << "Average of class " << i+1 << ": ";
+        for (int j = 0; j < classSize; j++)
+        {
+            averages[i] += scores[i][j];
+        }
+        averages[i] /= classSize;
+        cout << averages[i] << endl;
+    }
+    double max = averages[0];
+    for (int i = 1; i < classCount; i++)
+    {
+        if (averages[i] > max)
+        {
+            max = averages[i];
+            maxI = i;
+        }
+    }
+    cout << "Class " << maxI+1 << " has the highest average of " << max << endl;
+    delete[] averages;
+    for (int i = 0; i < classCount; i++)
+    {
+        delete[] scores[i];
+    }
+    delete[] scores;
+    return 0;
+}
