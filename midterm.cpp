6e0ec418f55df98327481f755ca4a5bfe388aa4e
Steven <osteve425@gmail.com>
2024-12-11 13:56:17 -0500

    problem one complete

---
+++ midterm.cpp
@@ -0,0 +1,13 @@
+//#1
+/*
+    void f(int f){
+        if(f!==4|| f!==5)return;
+        cout<<"Hello World \n";
+    }
+    int main(){
+        f(3);
+        f(4);
+        f(5);
+        //It will return nothing 
+    }
+*/
