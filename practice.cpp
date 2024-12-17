b6cbe74ddff0afb7ca3dacc692deffa2750bd2de
Steven-Ou <osteve425@gmail.com>
2024-12-17 00:17:02 -0500

    refixing

--- practice.cpp
+++ practice.cpp
@@ -20,6 +20,7 @@ vector<int> merge(vector<int> a, vector<int> b){
 }
 
 int main(){
+    //This way from line 25-35 is the only way for vscode
     vector<int> a;
     a.push_back(2);
     a.push_back(4);
@@ -28,7 +29,7 @@ int main(){
     vector<int>b;
     b.push_back(1);
     b.push_back(5);
-    b.push_back(3);
+    b.push_back(7);
     b.push_back(10);
     b.push_back(13);
     vector <int> c = merge(a,b);
