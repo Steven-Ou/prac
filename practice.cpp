fcd98851c45bf82faff80d06c22a024fc53e1f0c
Steven-Ou <osteve425@gmail.com>
2024-12-17 00:05:48 -0500

    forloop

--- practice.cpp
+++ practice.cpp
@@ -23,5 +23,9 @@ int main(){
     vector<int> a{2,4,6,8};
     vector<int> b{1,3,7,10,13};
     vector<int> c = merge(a,b);
+    for (int i = 0; i < c.size(); i++)
+    {
+        cout << c[i] << " ";
+    }
     return 0;
 }
