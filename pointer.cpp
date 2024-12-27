d8e744f1e240d341b6121640b5a30403bc57b11e
Steven-Ou <osteve425@gmail.com>
2024-12-27 16:02:41 +0800

    ending the line

--- pointer.cpp
+++ pointer.cpp
@@ -52,7 +52,7 @@ int main()
     int j = 40;
 
     // print the size of j
-    cout<<sizeof(j);
+    cout<<sizeof(j)<<endl;
     // declare another integer pointer, and name it ptr2
     int *ptr2; 
     // print the size of ptr2
